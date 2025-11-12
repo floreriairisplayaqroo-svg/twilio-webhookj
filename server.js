e904dc51bc5a16461e4bb056f56d0eaa4d0632de
floreriairisplayaqroo-svg <floreriairisplayaqroo@gmail.com>
2025-11-12 12:36:36 -0500

    Update server.js

--- server.js
+++ server.js
@@ -1,16 +1,24 @@
-import express from "express";
-import fetch from "node-fetch";
-
-const app = express();
-app.use(express.urlencoded({ extended: true }));
-
 app.post("/twilio", async (req, res) => {
   try {
-    await fetch("https://script.google.com/macros/s/TU_URL_SCRIPT/exec", {
+    // 🔍 Muestra lo que llega desde Twilio
+    console.log("Datos recibidos desde Twilio:", req.body);
+
+    // 📨 Convierte los datos a formato que Apps Script entiende
+    const params = new URLSearchParams();
+    for (const [key, value] of Object.entries(req.body)) {
+      params.append(key, value);
+    }
+
+    // 🚀 Envía los datos al Apps Script
+    const response = await fetch("https://script.google.com/macros/s/TU_URL_SCRIPT/exec", {
       method: "POST",
-      body: new URLSearchParams(req.body),
+      headers: { "Content-Type": "application/x-www-form-urlencoded" },
+      body: params.toString(),
     });
 
+    console.log("Respuesta de Apps Script:", await response.text());
+
+    // 📩 Respuesta para Twilio (debe ser XML)
     res
       .type("text/xml")
       .send(
@@ -21,13 +29,9 @@ app.post("/twilio", async (req, res) => {
     res
       .type("text/xml")
       .send(
-        '<?xml version="1.0" encoding="UTF-8"?><Response><Message>Error interno</Message></Response>'
+        '<?xml version="1.0" encoding="UTF-8"?><Response><Message>❌ Error interno</Message></Response>'
       );
   }
 });
-
-app.get("/", (req, res) => res.send("Webhook activo 🟢"));
-
-app.listen(3000, () => console.log("Servidor iniciado en puerto 3000"));
 
 
