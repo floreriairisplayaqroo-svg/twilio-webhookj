65c9d972e22240f4eae6f4092bbd564cd7d833fd
floreriairisplayaqroo-svg <floreriairisplayaqroo@gmail.com>
2025-11-12 10:45:30 -0500

    Update server.js

--- server.js
+++ server.js
@@ -6,11 +6,25 @@ app.use(express.urlencoded({ extended: true }));
 
 app.post("/twilio", async (req, res) => {
   try {
-    await fetch("https://script.google.com/macros/s/AKfycbxCJr4_nmMOkbNx6D6TvpYlvUn0qV3pQay4tRWp476PMke0b5bR0HffzbvGGV8W0b8cGA/exec", {
+    // 🔍 Muestra lo que llega desde Twilio
+    console.log("Datos recibidos desde Twilio:", req.body);
+
+    // 📨 Convierte los datos a formato que Apps Script entiende
+    const params = new URLSearchParams();
+    for (const [key, value] of Object.entries(req.body)) {
+      params.append(key, value);
+    }
+
+    // 🚀 Envía los datos al Apps Script
+    const response = await fetch("https://script.google.com/macros/s/AKfycbxCJr4_nmMOkbNx6D6TvpYlvUn0qV3pQay4tRWp476PMke0b5bR0HffzbvGGV8W0b8cGA/exec", {
       method: "POST",
-      body: new URLSearchParams(req.body),
+      headers: { "Content-Type": "application/x-www-form-urlencoded" },
+      body: params.toString(),
     });
 
+    console.log("Respuesta de Apps Script:", await response.text());
+
+    // 📩 Respuesta para Twilio (debe ser XML)
     res
       .type("text/xml")
       .send(
@@ -21,11 +35,13 @@ app.post("/twilio", async (req, res) => {
     res
       .type("text/xml")
       .send(
-        '<?xml version="1.0" encoding="UTF-8"?><Response><Message>Error interno</Message></Response>'
+        '<?xml version="1.0" encoding="UTF-8"?><Response><Message>❌ Error interno</Message></Response>'
       );
   }
 });
 
+
 app.get("/", (req, res) => res.send("Webhook activo 🟢"));
 
 app.listen(3000, () => console.log("Servidor iniciado en puerto 3000"));
+
